9ce2b8cc8b97fa54693b090451e5ded2ba1f1403
vinodgowda1477 <vinodgowda417@gmail.com>
2020-09-08 22:40:55 +0530

    initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathOpsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathOpsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6fa47b0d-145d-4e98-a97b-176fe2398c48")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using MathOpsLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MathOpsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_AddMethod()
+        { 
+            var bm = new MathOpsClass();
+            double res = bm.Add(10, 10);
+            Assert.AreEqual(res, 20);
+        }
+        [TestMethod]
+        public void Test_SubstractMethod()
+        {
+            var bm = new MathOpsClass();
+            double res = bm.Substract(10, 10);
+            Assert.AreEqual(res, 0);
+        }
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            var bm = new MathOpsClass();
+            double res = bm.divide(10, 5);
+            Assert.AreEqual(res, 2);
+        }
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            var bm = new MathOpsClass();
+            double res = bm.Multiply(10, 10);
+            Assert.AreEqual(res, 100);
+        }
+    }
+}
